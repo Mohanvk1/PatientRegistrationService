5f1ad438385dcf4791195d770fcc6782d44aa529
Mohanvk1 <mohan07.ade@gmail.com>
2025-05-16 07:18:59 -0500

    updated Patient Registartion Service

--- Models/Patient.cs
+++ Models/Patient.cs
@@ -5,10 +5,16 @@ public class Patient
     public string? LastName { get; set; }
     // public int Age { get; set; }
     // public string? Diagnosis { get; set; }
-    // public string? DateOfBirth { get; set; }
+     public DateTime DateOfBirth { get; set; }
     public string? Insurance { get; set; }
-    
-    // public long Phone{ get; set; }
+
+    public long Phone { get; set; }
+
+    public string? StreetAddress { get; set; }
+    public string? City { get; set; }
+    public string? State { get; set; }
+    public string? ZipCode { get; set; }
+    public string?Country{ get; set; }
 
     
 }
